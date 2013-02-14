cff6967a1f8a7755c474cf2d6150d5de10a0b87b
emokid250@gmail.com <emokid250@gmail.com@9c931025-5e0e-c7c7-95f7-f36e5c148c03>
2013-02-14 09:09:55 +0000

    First Subversion entry. Demo of using unit testing and coding to a interface.

---
+++ hello/src/com/geoffrey/hello/HelloWorldImp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.geoffrey.hello;
+
+/**
+ *
+ * @author 211026298
+ */
+public class HelloWorldImp implements HelloWorld {
+
+    public HelloWorldImp() {
+    }
+
+    @Override
+    public String getString() {
+        return "Hello World";
+    }
+    
+}
