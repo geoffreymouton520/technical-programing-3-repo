3a87bef5313cf3a90c2c70720af30981eb0b62f2
emokid250@gmail.com <emokid250@gmail.com@9c931025-5e0e-c7c7-95f7-f36e5c148c03>
2013-02-21 19:54:33 +0000

    Version 3 Add Libs AGAIN!!

---
+++ trunk/Assignment1/test/com/geoffrey/test/Assignment1Test.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.geoffrey.test;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ *
+ * @author geoffrey
+ */
+public class Assignment1Test {
+    
+    public Assignment1Test() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+}
