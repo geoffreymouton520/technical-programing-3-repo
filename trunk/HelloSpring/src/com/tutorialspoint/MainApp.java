3be34d4a46d548c0547a4fda72ee2f166b92e599
emokid250@gmail.com <emokid250@gmail.com@9c931025-5e0e-c7c7-95f7-f36e5c148c03>
2013-03-12 12:13:09 +0000

    Spring Tutorial Exercise 1

---
+++ trunk/HelloSpring/src/com/tutorialspoint/MainApp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tutorialspoint;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author 211026298
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+        obj.getMessage();
+    }
+}
