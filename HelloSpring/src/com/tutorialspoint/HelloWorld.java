3976ee8f8629164f8ad89e5ff5fd61a652d928ff
emokid250@gmail.com <emokid250@gmail.com@9c931025-5e0e-c7c7-95f7-f36e5c148c03>
2013-03-12 12:13:09 +0000

    Spring Tutorial Exercise 1

---
+++ HelloSpring/src/com/tutorialspoint/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tutorialspoint;
+
+/**
+ *
+ * @author 211026298
+ */
+public class HelloWorld {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void getMessage() {
+        System.out.println("Your Message : " + message);
+    }
+}
