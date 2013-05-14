ff494d91984af86e9c711bf889dd941d9a567139
emokid250@gmail.com <emokid250@gmail.com@9c931025-5e0e-c7c7-95f7-f36e5c148c03>
2013-05-14 07:38:43 +0000

    Added procedures

---
+++ trunk/Gymapp/src/java/com/geoffrey/gymapp/repository/JPAProcedures.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.geoffrey.gymapp.repository;
+
+/**
+ *
+ * @author 211026298
+ */
+public interface JPAProcedures {
+    public void createExercise();
+}
